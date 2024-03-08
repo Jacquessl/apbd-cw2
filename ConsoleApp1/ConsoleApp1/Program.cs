98e142ec108799211d6df29dc34b8d7d53b500ed
s27648 <mr.jakublitwin@gmail.com>
2024-03-08 14:58:17 +0100

    Dodano teraz juz poprawny average feature

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Modyfikacja 1");
-Console.WriteLine("Modyfikacja 2");
-Console.WriteLine("Modyfikacja 3");
-Console.WriteLine("Wymaganie 1");
-Console.WriteLine("Wymaganie 2");
+class MyClass
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Modyfikacja 1");
+        Console.WriteLine("Modyfikacja 2");
+        Console.WriteLine("Modyfikacja 3");
+        
+        Console.WriteLine("Wymaganie 1: " + featureAverage(new int[]{1,2,3,4,5,6}));
+        
+    }
+
+    static int featureAverage(int[] tab)
+    {
+        int sum = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            sum += tab[i];
+        }
+
+        return sum / tab.Length;
+    }
+}
+
+
